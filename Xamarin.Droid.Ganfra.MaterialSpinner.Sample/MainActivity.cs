b36c0f42fc1d63002e8fab6470ae0b251230f317
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2018-04-19 21:26:27 +0200

    Change namespace.

--- Xamarin.Droid.Ganfra.MaterialSpinner.Sample/MainActivity.cs
+++ Xamarin.Droid.Ganfra.MaterialSpinner.Sample/MainActivity.cs
@@ -3,7 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
-using FR.Ganfra.Materialspinner;
+using Fr.Ganfra.MaterialSpinner;
 using Java.Interop;
 
 namespace Nivaes.Materialspinner.Droid
